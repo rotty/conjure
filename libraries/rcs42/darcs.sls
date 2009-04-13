2391f23627037f00edeb9d2b47fe841d0499ee21
Andreas Rottmann <rotty@debian.org>
2009-04-13 19:04:50 +0200

    (rcs42 darcs): Don't rely on `run-process' searching $PATH
    Ignore-this: 7c2dc656cdd8868891e1b8df683033cf
    
    darcs-hash:20090413170450-b4420-a1f191716eba12bddc84b926f004f8911b9cf8c0.gz

--- libraries/rcs42/darcs.sls
+++ libraries/rcs42/darcs.sls
@@ -14,6 +14,7 @@
           (spells pathname)
           (spells filesys)
           (spells process)
+          (spells sysutils)
           (spells delimited-readers)
           (spells misc)
           (spells opt-args)
--- scheme/darcs.scm
+++ scheme/darcs.scm
@@ -1,7 +1,20 @@
 ;; -*- mode: scheme; scheme48-package: rcs42.darcs; -*-
 
+(define darcs-command
+  (or (find-exec-path "darcs")
+      (error 'darcs-command "darcs executable not found")))
+
 (define (run-darcs command . args)
-  (apply run-process #f "darcs" (symbol->string command) args))
+  (define (lose msg . irritants)
+    (apply error
+           (string-append "darcs command '" (symbol->string command) "' " msg)
+           irritants))
+  (receive (status sig)
+           (apply run-process #f darcs-command (symbol->string command) args)
+    (cond (sig
+           (lose "killed by signal" sig))
+          ((not (= status 0))
+           (lose "exited with non-zero status" status)))))
 
 (define (run-darcs/log command . args)
   (for-each display `("% darcs " ,command " " ,(string-join args " ") #\newline))
@@ -16,7 +29,7 @@
                                       (make-pathname #f '() #f)))
                      f))
               (receive (status sig filenames)
-                       (run-process/lines #f "darcs" "query" "files")
+                       (run-process/lines #f darcs-command "query" "files")
                 (if (= 0 status)
                     filenames
                     '()))))
@@ -48,7 +61,7 @@
   (define (lose msg . irritants)
     (apply error 'darcs-diff msg irritants))
   (receive (status sig lines)
-           (run-process/lines #f "darcs" "diff" "-u")
+           (run-process/lines #f darcs-command "diff" "-u")
     (cond (sig
            (lose "'darcs diff' killed by signal" sig))
           ((not (memv status '(0 1)))
