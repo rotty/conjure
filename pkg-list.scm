97937d71804c299a38a0279b8791bb67d3c57da6
Andreas Rottmann <a.rottmann@gmx.at>
2010-04-09 13:08:37 +0200

    Added pkg-list.scm

---
+++ pkg-list.scm
@@ -0,0 +1,11 @@
+(package (conjure (0))
+  (libraries
+   (sls -> "conjure"))
+  (depends
+   (prometheus)
+   (spells)
+   (srfi)))
+
+;; Local Variables:
+;; scheme-indent-styles: ((package 1))
+;; End:
