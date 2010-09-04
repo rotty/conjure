1022925555b0a0db978190bdd8a092f87600f16a
Andreas Rottmann <a.rottmann@gmx.at>
2010-09-05 00:44:46 +0200

    pkg-list.scm: bump version, add descriptive metadata

--- pkg-list.scm
+++ pkg-list.scm
@@ -1,13 +1,20 @@
-(package (conjure (0))
+(package (conjure (0 1 0))
+  (depends (srfi)
+           (wak-fmt)
+           (wak-irregex)
+           (wak-foof-loop)
+           (wak-prometheus)
+           (spells))
+  
+  (synopsis "Scheme make replacement")
+  (description
+   "conjure is a library, including a thin domain specific"
+   "language layer, that allows expressing build instructions"
+   "such as traditionally found in Makefiles.")
+  (homepage "http://github.com/rotty/conjure")
+  
   (libraries
-   (sls -> "conjure"))
-  (depends
-   (wak-fmt)
-   (wak-irregex)
-   (wak-foof-loop)
-   (wak-prometheus)
-   (spells)
-   (srfi)))
+   (sls -> "conjure")))
 
 ;; Local Variables:
 ;; scheme-indent-styles: ((package 1))
